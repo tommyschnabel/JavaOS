31a9c02b4452c22072095f0ca919e56f41554912
Alex Googe <alexgoogethera@gmail.com>
2016-02-28 19:57:15 -0500

    Created the queues in a LinkedList. Started working on the FIFO for the short term. Made a DefaultShortTerm file.

--- src/main/java/edu/ksu/operatingsystems/javaos/scheduling/ShortTermScheduler.java
+++ src/main/java/edu/ksu/operatingsystems/javaos/scheduling/ShortTermScheduler.java
@@ -1,4 +1,33 @@
 package edu.ksu.operatingsystems.javaos.scheduling;
 
+import java.util.*;
+import edu.ksu.operatingsystems.javaos.storage.Ram;
+
 public interface ShortTermScheduler {
+    
+    /**
+     * Schedules all processes in need of scheduling
+     * @param pcbArray The array of PCBs in the RAM
+     */
+    public void doScheduling(ProcessControlBlock[] pcbArray);
+    
+    /**
+     * Adds a pcb to the ready queue
+     * @param pcb The pcb to be added to the readyQueue
+     */
+    public void addToReadyQueue(ProcessControlBlock pcb);
+    
+    /**
+     * Adds instruction and data set to memory
+     * @param pcb The pcb to be added to the waitQueue
+     */
+    public void addToWaitQueue(ProcessControlBlock pcb);
+    
+    
+    /**
+     * Finds the processes that need to be put in the ready queue
+     * @param ram The ram from which we will grab the processes
+     * @return 
+     */
+    public ProcessControlBlock[] getProcesses(Ram ram);
 }
